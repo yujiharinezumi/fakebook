3247ce85e9914de237e2f28c1ab5eab823e0585a
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-07 14:41:41 +0900

    added user function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:new,:create,:show]
   resources :blogs do
     collection do
       post :confirm
-    end 
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
