72a4206ff6569e4acb09ad0157671127f81d59f1
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-07 15:45:43 +0900

    added session function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new,:create,:show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   resources :blogs do
     collection do
       post :confirm
