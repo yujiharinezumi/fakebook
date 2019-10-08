e7e897d0cd64aa5b72673ac897c6374c537af0be
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-08 12:02:41 +0900

    setting timezone

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module SnsClone
 
      config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
 
+     config.time_zone = 'Tokyo'
+     config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
