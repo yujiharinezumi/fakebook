33b279de8b52c82fdec5917c62f414475708e4d7
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-08 14:23:11 +0900

    added header design for aplication.html&css

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # storage :fog
 
 
-  process :resize_to_limit => [300, 300]
+  process :resize_to_limit => [340, 340]
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
