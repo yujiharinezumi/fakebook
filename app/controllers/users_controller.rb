b1d37c2ddbc40b851f70509a3a4fec3d48ee4984
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-09 11:31:35 +0900

    added user edit&update functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show,:edit,:update,:destroy]
+  def index
+  end
+
   def new
      @user = User.new
   end
@@ -17,11 +21,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+      @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to blogs_path,notice:"ユーザー情報を編集しました！"
+    else
+      render :edit
+    end
+  end
+
   private
 
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def set_user
+      @user = User.find(params[:id])
   end
 end
